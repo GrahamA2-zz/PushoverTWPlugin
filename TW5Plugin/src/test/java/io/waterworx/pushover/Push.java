ad84b17ad85484630a0822a2e6b57e25d2d51560
Graham Allan <graham.allan@bcs.org.uk>
2015-11-03 14:56:33 +0000

    Fixed TW5 bug

--- TW5Plugin/src/test/java/io/waterworx/pushover/Push.java
+++ TW5Plugin/src/test/java/io/waterworx/pushover/Push.java
@@ -21,7 +21,7 @@ public class Push {
 		pushover = injector.getInstance(PushoverService.class);
 
 		Push p = new Push();
-		p.sendMessage();
+		p.sendLongMessage();
 	}
 
 	private Properties prop = new Properties();
@@ -31,6 +31,28 @@ public class Push {
 		String userID = prop.getProperty("userID");
 		try{
 			String status = pushover.push(apiToken, "ut2BN71hqXPQwJ1v6vK2c5Xqg7LkRV", "Critical Message");
+			System.out.println(status);
+		} catch (Exception e){
+			System.err.println(e);
+		}
+
+	}
+	
+	
+	private void sendLongMessage() {
+		String apiToken = prop.getProperty("apiToken");
+		String userID = prop.getProperty("userID");
+		try{
+			String status = pushover.push("ad8xQzk4aizQZwxnwyCvuwXjjZsf39"
+										 ,"uQgzsYM37Di4AK4e8cRg2KEWQCGYMn"
+										 ,"Test 3"
+										 ,"Nexus5x"
+										 ,"Message Title"
+										 ,"http://www.theregister.co.uk/"
+										 ,"http://www.theregister.co.uk/"
+										 , 1
+										 , 1446556825L
+										 , "Bugle");
 			System.out.println(status);
 		} catch (Exception e){
 			System.err.println(e);
