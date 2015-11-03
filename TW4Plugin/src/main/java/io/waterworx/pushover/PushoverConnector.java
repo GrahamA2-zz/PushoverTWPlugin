4d719f8c130c55dba6ebc4073264e5e7697c5b21
Graham Allan <graham.allan@bcs.org.uk>
2015-11-03 14:57:14 +0000

    Fixed TW5 bug

--- TW4Plugin/src/main/java/io/waterworx/pushover/PushoverConnector.java
+++ TW4Plugin/src/main/java/io/waterworx/pushover/PushoverConnector.java
@@ -12,100 +12,73 @@ import com.thingworx.metadata.annotations.ThingworxServiceParameter;
 import com.thingworx.metadata.annotations.ThingworxServiceResult;
 import com.thingworx.things.Thing;
 
+//@formatter:off
+@ThingworxConfigurationTableDefinitions(tables = {
+		@com.thingworx.metadata.annotations.ThingworxConfigurationTableDefinition(name = "PushoverConnector", description = "Pushover Application Connection Parameters", isMultiRow = false, ordinal = 0, dataShape = @com.thingworx.metadata.annotations.ThingworxDataShapeDefinition(fields = {
+				@com.thingworx.metadata.annotations.ThingworxFieldDefinition(ordinal = 0, name = "APIToken", description = "your application's API token", baseType = "STRING", aspects = {
+						"defaultValue:your.token", "friendlyName:API Token" }),
 
-@ThingworxConfigurationTableDefinitions( tables = { @com.thingworx.metadata.annotations.ThingworxConfigurationTableDefinition(	
-		name = "PushoverConnector",
-		description = "Pushover Application Connection Parameters",
-		isMultiRow = false,
-		ordinal = 0,
-		dataShape = @com.thingworx.metadata.annotations.ThingworxDataShapeDefinition(
-				fields = { @com.thingworx.metadata.annotations.ThingworxFieldDefinition( ordinal = 0,
-				                                                                         name = "APIToken",
-				                                                                         description = "your application's API token",
-				                                                                         baseType = "STRING",
-				                                                                         aspects = { "defaultValue:your.token", "friendlyName:API Token" }),
-					
+		}) ) })
 
-})) })
-
-
-public class PushoverConnector extends Thing{
+public class PushoverConnector extends Thing {
 
 	private static final long serialVersionUID = 1L;
 	private final PushoverService pushover;
 	private String apiToken = "";
-	
-	public PushoverConnector(){
+
+	public PushoverConnector() {
 		Injector injector = Guice.createInjector(new AppInjector());
 		pushover = injector.getInstance(PushoverService.class);
 	}
-	
+
 	@Override
 	protected void initializeThing() throws Exception {
-		//May need to move pushover here 
+		// May need to move pushover here
 		this.apiToken = ((String) getConfigurationData().getValue("PushoverConnector", "APIToken"));
 	}
-	
+
 	@ThingworxServiceDefinition(name = "PushMessageShort", description = "Push a notification to a user or group")
 	@ThingworxServiceResult(name = "Result", description = "Result", baseType = "STRING")
-	public String PushMessageShort( @ThingworxServiceParameter( name = "user",
-	                                                       description = "the user/group key (not e-mail address) of your user (or you)",
-	                                                       baseType = "STRING", aspects = { "defaultValue:" }) String userID,
-							    @ThingworxServiceParameter( name = "message",
-								                            description = "your message",
-								                            baseType = "STRING",
-		 						                            aspects = { "defaultValue:" }) String message) throws Exception {
-		
+	public String PushMessageShort(
+			@ThingworxServiceParameter(name = "user", description = "the user/group key (not e-mail address) of your user (or you)", baseType = "STRING", aspects = {
+					"defaultValue:" }) String userID,
+			@ThingworxServiceParameter(name = "message", description = "your message", baseType = "STRING", aspects = {
+					"defaultValue:" }) String message) throws Exception {
+
 		String status = pushover.push(apiToken, userID, message);
 		return status;
 	}
-	
+
 	@ThingworxServiceDefinition(name = "PushMessageLong", description = "Push a notification to a user or group includeing a title and URL")
 	@ThingworxServiceResult(name = "Result", description = "Result", baseType = "STRING")
-	public String PushMessageLong( @ThingworxServiceParameter( name = "user",
-            															  description = "the user/group key (not e-mail address) of your user (or you)",
-            															  baseType = "STRING", aspects = { "defaultValue:" }) String userID,
-            								  @ThingworxServiceParameter( name = "message",
-            															  description = "Your message",
-            															  baseType = "STRING",
-            															  aspects = { "defaultValue:" }) String message,
-            								  @ThingworxServiceParameter( name = "device",
-            															  description = "Your user's device name to send the message directly to that device, rather than all of the user's devices (multiple devices may be separated by a comma)",
-            															  baseType = "STRING",
-            															  aspects = { "defaultValue:" }) String device,
-            				            	  @ThingworxServiceParameter( name = "title",
-            				            						          description = "Your message's title, otherwise your app's name is used",
-            				            								  baseType = "STRING",
-            				            								  aspects = { "defaultValue:" }) String title,
-            				            	  @ThingworxServiceParameter( name = "url",
-            				            						          description = "A supplementary URL to show with your message",
-            				            								  baseType = "STRING",
-            				            								  aspects = { "defaultValue:" }) String url,
-            				            	  @ThingworxServiceParameter( name = "url_title",
-            				            						          description = "A title for your supplementary URL, otherwise just the URL is shown",
-            				            								  baseType = "STRING",
-            				            								  aspects = { "defaultValue:" }) String url_title,
-            								  @ThingworxServiceParameter( name = "priority",
-            															  description = "Send as -2 to generate no notification/alert, -1 to always send as a quiet notification, 1 to display as high-priority and bypass the user's quiet hours, or 2 to also require confirmation from the user",
-            															  baseType = "STRING",
-            															  aspects = { "defaultValue:","0" }) String priority,
-            								  @ThingworxServiceParameter( name = "timestamp",
-            															  description = "A Unix timestamp of your message's date and time to display to the user, rather than the time your message is received by our API",
-            															  baseType = "STRING",
-            															  aspects = { "defaultValue:" }) String timestamp,
-            								  @ThingworxServiceParameter( name = "sound",
-            															  description = "The name of one of the sounds supported by device clients to override the user's default sound choice",
-            															  baseType = "STRING",
-            															  aspects = { "defaultValue:","pushover" }) String sound   
-            				            								  ) throws Exception {
-			Integer validatePriorty = Integer.parseInt(priority);
-			Long validateTimestamp = Long.parseLong(timestamp);
-			if (validateTimestamp == 0){
-				Date now = new Date();  	
-				validateTimestamp = new Long(now.getTime()/1000);			
-			}
-		    String status = pushover.push(apiToken, userID, message,device,title,url,url_title,validatePriorty,validateTimestamp,sound);
-			return status;
+	public String PushMessageFull(
+			@ThingworxServiceParameter(name = "user", description = "the user/group key (not e-mail address) of your user (or you)", baseType = "STRING", aspects = {
+					"defaultValue:" }) String userID,
+			@ThingworxServiceParameter(name = "message", description = "Your message", baseType = "STRING", aspects = {
+					"defaultValue:" }) String message,
+			@ThingworxServiceParameter(name = "device", description = "Your user's device name to send the message directly to that device, rather than all of the user's devices (multiple devices may be separated by a comma)", baseType = "STRING", aspects = {
+					"defaultValue:" }) String device,
+			@ThingworxServiceParameter(name = "title", description = "Your message's title, otherwise your app's name is used", baseType = "STRING", aspects = {
+					"defaultValue:" }) String title,
+			@ThingworxServiceParameter(name = "url", description = "A supplementary URL to show with your message", baseType = "STRING", aspects = {
+					"defaultValue:" }) String url,
+			@ThingworxServiceParameter(name = "url_title", description = "A title for your supplementary URL, otherwise just the URL is shown", baseType = "STRING", aspects = {
+					"defaultValue:" }) String url_title,
+			@ThingworxServiceParameter(name = "priority", description = "Send as -2 to generate no notification/alert, -1 to always send as a quiet notification, 1 to display as high-priority and bypass the user's quiet hours, or 2 to also require confirmation from the user", baseType = "STRING", aspects = {
+					"defaultValue:", "0" }) String priority,
+			@ThingworxServiceParameter(name = "timestamp", description = "A Unix timestamp of your message's date and time to display to the user, rather than the time your message is received by our API", baseType = "STRING", aspects = {
+					"defaultValue:" }) String timestamp,
+			@ThingworxServiceParameter(name = "sound", description = "The name of one of the sounds supported by device clients to override the user's default sound choice", baseType = "STRING", aspects = {
+					"defaultValue:", "pushover" }) String sound) throws Exception {
+		Integer validatePriorty = Integer.parseInt(priority);
+		Long validateTimestamp = Long.parseLong(timestamp);
+		if (validateTimestamp == 0) {
+			Date now = new Date();
+			validateTimestamp = new Long(now.getTime() / 1000);
+		}
+		String status = pushover.push(apiToken, userID, message, device, title, url, url_title, validatePriorty,
+				validateTimestamp, sound);
+		return status;
 	}
 }
-//@formatter:on
+// @formatter:on
--- readme.md
+++ readme.md
@@ -2,15 +2,23 @@
 
 Send notifications from [Thingworx](http://www.thingworx.com) to [Pushover](https://pushover.net/)
 
-This project comes in two version for Thingworx 4 and Thingworx 5 - I don;t currently have access to the TW6 jars to test that version.
+This project comes in two version for Thingworx 4 and Thingworx 5 - I don’t currently have access to the TW6 jars to test that version.
 
 To build a Thingworx plugin you will need to place the thingworx.jar into the lib folder 
 
     /TW4Plugin/lib...	thingworx-all-4.2.0.jar
     /TW5Plugin/lib...	thingworx-common-5.4.0.b445.jar
 
-These .jars can't be included in this project as I don't have the authors permision.
+These .jars can't be included in this project as I don't have the authors permission.
 
 This project uses Gradle (http://gradle.org/)
 
 To build the plugins run: `gradle thingworxDist`  
+
+#  Installation
+Build the plugin using the command `gradle thingworxDist`
+The plugin will be created in \PushoverTWPlugin\TWxPlugin\build\distributions
+Open Thingworx and import the plugin, the plugin creates a new Thing Template
+Create a new Thing
+Go to the configuration page and enter your Applications API token (this is not the same as your User Key).  If you don't have an Application registered then you will need to register one at https://pushover.net/apps/build
+To send a message go to Services and selected the service Enter your User Key or Group Key
